8bf31670e75d758868d27c899da5e5fab4bef3ec
Victoria Landron <viclandron@gmail.com>
2021-01-07 11:50:10 -0500

    adding markup of contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ContactForm(){
+    return (
+        <section>
+          <h1>Contact me</h1>
+          <form id="contact-form">
+            // name input
+            // email input
+            // message text area
+          </form>
+        </section>
+      )
+}
+
+export default ContactForm;
